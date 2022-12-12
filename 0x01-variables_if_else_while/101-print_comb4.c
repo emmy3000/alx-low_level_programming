04a6145f9ac72a3cfe03bb640612cca9b815e83e
Emeka Emodi <emodiemeka@gmail.com>
2022-12-11 23:10:29 -0800

    a program that prints all possible different combinations of three digits.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print all possible comninations of three different digits in an
+ * ascending order separated by a comma followed by a space
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
+
+				if  (i == 7 && j == 8 && k == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
