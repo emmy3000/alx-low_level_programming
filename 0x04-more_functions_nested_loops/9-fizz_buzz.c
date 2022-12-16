7b9941f18236176e068a7d3755b6a6d80a549fb1
Emeka Emodi <emodiemeka@gmail.com>
2022-12-16 08:28:45 -0800

    FizzBuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main - program entry point
- * Description: print numbers 1 - 100
- * multiples of 3 output Fizz, multiples of 5
- * output Buzz, multiples of 15 out FizzBuzz
+ *
+ * Description: prints numbers 1 to 100
+ * multiples of 3 output Fizz, multiples of 5 output Buzz,
+ * multiples of 15 output FizzBuzz
  *
  * Return: 0
 */
@@ -15,7 +16,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0);
+		if (i % 15 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
@@ -26,6 +27,6 @@ int main(void)
 		if (i < 100)
 			printf(" ");
 	}
-	printf("\n");
+	printf()"\n"0;
 	return (0);
 }
