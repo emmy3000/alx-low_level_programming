e75b431eb262afee092cad55154363a1f5c6d6e4
Emeka Emodi <emodiemeka@gmail.com>
2022-12-19 16:37:00 -0800

    function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string followed by new line
+ * @str: string-pointer parameter check
+ *
+ * Return: 0
+*/
+
+void _puts(char *str)
+{
+	puts(str);
+	puts("\n");
+}
