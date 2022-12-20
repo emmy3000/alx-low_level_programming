95a313ebde6566a5b0f4edec5fef61dc09843d73
Emeka Emodi <emodiemeka@gmail.com>
2022-12-20 02:47:16 -0800

    function that prints second half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,10 +18,9 @@ void puts_half(char *str)
 	else
 		half_length = i / 2;
 	half_length++;
-	i = half_length;
 
-	while (str[i] != '\0')
+	for (i = half_length; str[i] != '\0'; i++)
 		_putchar(str[i]);
-		i++;
 	_putchar('\n');
+
 }
