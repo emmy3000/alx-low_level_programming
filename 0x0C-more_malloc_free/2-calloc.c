e0e6e7fc61f13c57e0320e00f8a10729f8d3dcd9
Emeka Emodi <emodiemeka@gmail.com>
2023-01-12 20:34:29 -0800

    function allocates memory using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = malloc(nmemb * size)
+	arr = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
