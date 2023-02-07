1cc7a07b6f97efe6ef7f02a4ae4166dd48d689b0
emmy3000 <emodiemeka@gmail.com>
2023-02-06 18:59:32 -0800

    function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -3,27 +3,22 @@
 #include "main.h"
 
 /**
- * main - checks the code
+ * main - check the code
  *
- * @argc: args counter
- * @argv: args array
- *
- * Return:0 Success
+ * Return: Always 0.
  */
-
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	ssize_t n;
+    ssize_t n;
 
-	if (argc != 2)
-	{
-		dprintf(2, "Usage: %s filename\n", argv[0]);
-		exit(1);
-	}
-	n = read_textfile(argv[1], 114);
-	printf("\n(printed chars: %li)\n", n);
-	n = read_textfile(argv[1], 1024);
-	printf("\n(printed chars: %li)\n", n);
-
-	return (0);
+    if (ac != 2)
+    {
+        dprintf(2, "Usage: %s filename\n", av[0]);
+        exit(1);
+    }
+    n = read_textfile(av[1], 114);
+    printf("\n(printed chars: %li)\n", n);
+    n = read_textfile(av[1], 1024);
+    printf("\n(printed chars: %li)\n", n);
+    return (0);
 }
--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,7 +15,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	aaize_t o, r, w;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
