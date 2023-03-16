76e8fe06c21779042d0dd1883e56ab98f9cff14a
emmy3000 <emodiemeka@gmail.com>
2023-03-16 14:45:34 -0700

    Task Updated

---
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Program that finds the largest palindrome number\
+        that is a product of two 3-digit numbers."""
+
+
+def ispalindrome(n):
+    string = str(n)
+    return string == string[::-1]
+
+
+array = []
+for i in range(999, 99, -1):
+    for j in range(i, 99, -1):
+        if ispalindrome(i * j):
+            array.append(i * j)
+print(max(array))
