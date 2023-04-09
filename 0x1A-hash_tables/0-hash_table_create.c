48cc964d4f6b1597487e6c2c873375a3c89237f0
emmy3000 <emodiemeka@gmail.com>
2023-04-08 20:01:38 -0700

    Create a new hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,12 +4,12 @@
 
 
 /**
- * hash_table_create: program create a new hash table.
- *
- * @size: size of the hash table.
- *
- * Returns: a pointer to a new hash table
- */
+	* hash_table_create: program create a new hash table.
+	*
+	* @size: size of the hash table.
+	*
+	* Returns: a pointer to a new hash table
+	*/
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -17,17 +17,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	
 	/* new hash table instantiation */
-	table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	
 	if (table == NULL)
-		return NULL;
+	return NULL;
 
 	/* Initializing hash table members */
 	table->size = size;
-	table->array = (hash_node_t**)malloc(sizeof(hash_node_t*) * size);
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 
 	for (i = 0; i < table->size; i++)
-		table->array[i] = NULL;
+	table->array[i] = NULL;
 
 	return table;
 }
