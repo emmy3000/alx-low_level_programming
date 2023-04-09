5460ab9a7d8608b570052a12c2fd194d26c57262
emmy3000 <emodiemeka@gmail.com>
2023-04-08 20:48:09 -0700

    Create a new hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,30 +4,38 @@
 
 
 /**
-	* hash_table_create: program create a new hash table.
-	*
-	* @size: size of the hash table.
-	*
-	* Returns: a pointer to a new hash table
-	*/
-
+ * hash_table_create - program creates a new hash table
+ *
+ * @size: size of the hash table
+ *
+ * Return: pointer to a newly created hash table or NULL on failure
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* table;
+	hash_table_t *table;
 	unsigned long int i;
-	
-	/* new hash table instantiation */
-	table = (hash_table_t *)malloc(sizeof(hash_table_t));
-	
-	if (table == NULL)
-		return NULL;
 
-	/* Initializing hash table members */
+	/* New hash table instantiation */
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	/* Initialize hash table members */
 	table->size = size;
 	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 
-	for (i = 0; i < table->size; i++)
-		table->array[i] = NULL;
+	/* Iterate and initialize overflow buckets to NULL */
+	for (i = 0; i < size; i++)
+	{
+		if (table->array != NULL)
+			table->array[i] = NULL;
+		else
+		{
+			free(table->array);
+			return (NULL);
+		}
+	}
 
-	return table;
+	return (table);
 }
