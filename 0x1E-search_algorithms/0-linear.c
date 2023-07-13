684062f811e5279a13ad1a869c8a6f42acbc0869
Emeka Emodi <emodiemeka@gmail.com>
2023-07-12 18:16:02 -0700

    Prototype Definition: Linear Search Algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+
+/*
+ * linear_search - Searches for a value in an array
+ * using linear search algorithm
+ *
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: Value to search for.
+ *
+ * Description: This function performs a linear search on the given array
+ * to find the first occurrence of the specified value. It prints each
+ * element in the array that is checked during the search.
+ *
+ * Return: The first index where the value is located, or -1 if not found.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
